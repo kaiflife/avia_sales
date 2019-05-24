2ceff43fe83d0f2cbc441cc15f4da39765d4af69
pashok <makesomecode@gmail.com>
2019-05-24 19:53:25 +0300

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/logo.css 	new file:   src/mobileGallary.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.css";
 import app from "./App.css";
+import gallary from "./mobileGallary.css";
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -43,7 +44,51 @@ function App() {
 				<div className='input-city'>
 					<p />
 				</div>
+				<div className='date'>
+					<div className='date-to' />
+					<div className='date-from' />
+				</div>
+				<div className='ticket-type'>
+					<div className='selector-ticket' />
+				</div>
+				<div className='find-tickets'>
+					<p>Найти билеты</p>
+				</div>
 			</header>{" "}
+			<main className='App-main'>
+				<div className='popular-flights'>
+					<div className='logo-way' />
+					<p>Популярные направления перелётов из города</p>
+					<div className='popular-city' />
+				</div>
+				<div className='travel-selector'>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+					<div className='travel-item'>
+						<p>Text</p>
+					</div>
+				</div>
+				<div className='Gallary'>
+					<div className='photo'>
+						<img />
+						<div className='ticket-info' />
+					</div>
+					/>
+				</div>
+			</main>
 		</div>
 	);
 }
