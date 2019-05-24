fa01f4dfd7b3f3185c2e0da3fc7edee3d847941e
pashok <makesomecode@gmail.com>
2019-05-24 03:04:13 +0300

    'lol'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.css";
+import app from "./App.css";
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div className='logo' />
+				<div className='header-text'>
+					<p />
+				</div>
+				<div className='selector-city'>
+					<p />
+					<div className='selector'>
+						<div />
+						<div />
+					</div>
+				</div>
+				<div className='input-city'>
+					<p />
+				</div>
+			</header>{" "}
+		</div>
+	);
 }
 
 export default App;
