eb22531bcd2ef982cb68273a3ef47c454dcdd977
pashok <makesomecode@gmail.com>
2019-05-28 17:18:38 +0300

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -187,29 +187,120 @@ function App() {
 					<div className='logo-way' />
 					<p>Лучшие цены на авиабилеты за последний месяц</p>
 				</div>
-				<div className='flights'>
-					<div className='country'>
-						<div className='country-icon' />
-						<div className='country-from'>
-							<p>Симферополь (Крым)</p>
-							<p>КРЫМ</p>
+				<div className='country-tickets'>
+					<div className='flights'>
+						<div className='country'>
+							<div className='country-icon' />
+							<div className='country-from'>
+								<p>Симферополь (Крым)</p>
+								<p>КРЫМ</p>
+							</div>
+						</div>
+
+						<div className='country-to'>
+							<div className='country-name'>
+								<p>Из Москвы</p>
+								<p>Из Санкт-Петербурга</p>
+								<p>Из Новосибирска</p>
+								<p>Из Екатеринбурга</p>
+								<p>Из Челябинска</p>
+							</div>
+							<div className='country-cost'>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 15 127 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+							</div>
 						</div>
 					</div>
-
-					<div className='country-to'>
-						<div className='country-name'>
-							<p>Из Москвы</p>
-							<p>Из Санкт-Петербурга</p>
-							<p>Из Новосибирска</p>
-							<p>Из Екатеринбурга</p>
-							<p>Из Челябинска</p>
+					<div className='flights'>
+						<div className='country'>
+							<div className='country-icon' />
+							<div className='country-from'>
+								<p>Симферополь (Крым)</p>
+								<p>КРЫМ</p>
+							</div>
 						</div>
-						<div className='country-cost'>
-							<p>от 4 813 ₽</p>
-							<p>от 4 813 ₽</p>
-							<p>от 15 127 ₽</p>
-							<p>от 4 813 ₽</p>
-							<p>от 4 813 ₽</p>
+
+						<div className='country-to'>
+							<div className='country-name'>
+								<p>Из Москвы</p>
+								<p>Из Санкт-Петербурга</p>
+								<p>Из Новосибирска</p>
+								<p>Из Екатеринбурга</p>
+								<p>Из Челябинска</p>
+							</div>
+							<div className='country-cost'>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 15 127 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+							</div>
+						</div>
+					</div>
+					<div className='flights'>
+						<div className='country'>
+							<div className='country-icon' />
+							<div className='country-from'>
+								<p>Симферополь (Крым)</p>
+								<p>КРЫМ</p>
+							</div>
+						</div>
+
+						<div className='country-to'>
+							<div className='country-name'>
+								<p>Из Москвы</p>
+								<p>Из Санкт-Петербурга</p>
+								<p>Из Новосибирска</p>
+								<p>Из Екатеринбурга</p>
+								<p>Из Челябинска</p>
+							</div>
+							<div className='country-cost'>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 15 127 ₽</p>
+								<p>от 4 813 ₽</p>
+								<p>от 4 813 ₽</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='adv-one'>
+					<p>
+						Мы знаем, где купить авиабилеты дешево. Билеты на самолет в 220
+						стран мира. Поиск и сравнение цен на авиабилеты среди 100 агентств и
+						728 авиакомпаний.
+					</p>
+					<p>
+						Цены, найденные пользователями за последние 48 часов, не являются
+						офертой.
+					</p>
+				</div>
+				<div className='Special-offers'>
+					<p>Спецпредложения на авиабилеты</p>
+					<div className='offer'>
+						<div className='offer-header'>
+							<p>Билеты от 499 рублей</p>
+						</div>
+						<div className='offer-main'>
+							<div>
+								<p>POBEDA</p>
+								<div>
+									<p>от 499 Р</p>
+									<p>Осталось 45 дней</p>
+								</div>
+							</div>
+							<div>
+								<p>
+									Билеты от 499 рублей! Специальное предложение от авиакомпании
+									Победа
+								</p>
+							</div>
+							<div>
+								<button>Узнать подробнее</button>
+							</div>
 						</div>
 					</div>
 				</div>
